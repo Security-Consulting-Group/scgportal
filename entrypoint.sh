09bd26c367f90aefd1fa1ef625a7ca7dc0d8abdb
ismgonza <ijgr11@gmail.com>
2024-10-11 21:08:58 -0600

    updating variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py migrate --settings=core.settings.production
 echo '\n======> Running server...'
 gunicorn --env DJANGO_SETTINGS_MODULE=core.settings.production core.wsgi:application --bind 0.0.0.0:8000 --workers 1
 
-# uwsgi --http :8000 --module config.wsgi:application --env DJANGO_SETTINGS_MODULE=config.settings.production --master --workers 4 --enable-threads --socket /tmp/uwsgi.sock --chmod-socket=664 --vacuum --die-on-term --thunder-lock
+# uwsgi --http :8000 --module core.wsgi:application --env DJANGO_SETTINGS_MODULE=core.settings.production --master --workers 4 --enable-threads --socket /tmp/uwsgi.sock --chmod-socket=664 --vacuum --die-on-term --thunder-lock
