8f22193956fb8ece80a7b99a01ed5058ce2627e6
ismgonza <ijgr11@gmail.com>
2024-10-12 00:45:45 -0600

    console email

--- core/settings/production.py
+++ core/settings/production.py
@@ -54,8 +54,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # CHECK FOR STORAGE
 # https://docs.djangoproject.com/en/5.0/ref/settings/#std-setting-STORAGES
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', default='')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
